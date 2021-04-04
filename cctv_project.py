7f116d2e59cb6ca5a9842e007634c5f0d62d7379
Lee-J-M <81926108+Lee-J-M@users.noreply.github.com>
2021-04-05 05:56:02 +0900

    Update cctv_project.py
    video version

--- cctv_project.py
+++ cctv_project.py
@@ -49,28 +49,11 @@ while True:
     print "Motion detected!"
 
     try:
-      camera.start_preview()
-      camera.capture('/home/pi/image%s.jpg' % counter)
-      camera.stop_preview()
-      path = "/home/pi/image%s.jpg" % counter  
-      part = MIMEBase("application", "octet-stream")
-      part.set_payload(open(path,'rb').read())
-      encoders.encode_base64(part)
-      part.add_header('Content-Disposition','attachment; filename = "%s"' %os.path.basename(path))
-      msg.attach(part)
-      google_server.sendmail(me, you , msg.as_string())
-      push = pb.push_note("[CAUTION!!]", "Motion Detected!")
-      time.sleep(2)
-      push = pb.push_link("Click here to check CCTV " ,"https://mail.google.com/mail/u/0/?ogbl#inbox")
-      counter = counter + 1
-
-      # CCTV VIDEO  #
+      ##cctv _ picture ##
       #camera.start_preview()
-      #camera.start_recording('/home/pi/video%s.h264' % counter)
-      #time.sleep(5)
-      #camera.stop_recording()
+      #camera.capture('/home/pi/image%s.jpg' % counter)
       #camera.stop_preview()
-      #path = '/home/pi/video%s.h264' % counter
+      #path = "/home/pi/image%s.jpg" % counter  
       #part = MIMEBase("application", "octet-stream")
       #part.set_payload(open(path,'rb').read())
       #encoders.encode_base64(part)
@@ -78,9 +61,27 @@ while True:
       #msg.attach(part)
       #google_server.sendmail(me, you , msg.as_string())
       #push = pb.push_note("[CAUTION!!]", "Motion Detected!")
-      #time.sleep(5)
+      #time.sleep(2)
       #push = pb.push_link("Click here to check CCTV " ,"https://mail.google.com/mail/u/0/?ogbl#inbox")
       #counter = counter + 1
+
+      # CCTV VIDEO  #
+      camera.start_preview()
+      camera.start_recording('/home/pi/video%s.h264' % counter)
+      time.sleep(5)
+      camera.stop_recording()
+      camera.stop_preview()
+      path = '/home/pi/video%s.h264' % counter
+      part = MIMEBase("application", "octet-stream")
+      part.set_payload(open(path,'rb').read())
+      encoders.encode_base64(part)
+      part.add_header('Content-Disposition','attachment; filename = "%s"' %os.path.basename(path))
+      msg.attach(part)
+      google_server.sendmail(me, you , msg.as_string())
+      push = pb.push_note("[CAUTION!!]", "Motion Detected!")
+      time.sleep(5)
+      push = pb.push_link("Click here to check CCTV " ,"https://mail.google.com/mail/u/0/?ogbl#inbox")
+      counter = counter + 1
 
     except:
       camera.stop_preview()
